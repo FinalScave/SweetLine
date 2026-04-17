2ee2bc8ea334d0d83be4ad22baa83af29136582c
Scave <iscave@163.com>
2026-04-17 23:20:13 +0800

    fix(syntax): unify compile error codes across bindings

---
+++ platform/Apple/Sources/SweetLineCoreInternal/model/SyntaxCompileError.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+public struct SyntaxCompileError: Error, Equatable {
+    public static let errJSONPropertyMissed = -1
+    public static let errJSONPropertyInvalid = -2
+    public static let errPatternInvalid = -3
+    public static let errStateInvalid = -4
+    public static let errJSONInvalid = -5
+    public static let errFileNotExists = -6
+    public static let errFileInvalid = -7
+    public static let errImportSyntaxNotFound = -8
+
+    public let errorCode: Int
+    public let message: String
+
+    public init(errorCode: Int, message: String) {
+        self.errorCode = errorCode
+        self.message = message
+    }
+}
